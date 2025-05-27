6d03b6cb2c82fb6563f137f75f03d93d1e71c1fe
StrataEdition <strata.edition@gmail.com>
2025-05-27 15:48:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-const modal = document.getElementById("modal");
-const openBtn = document.getElementById("moreInfoBtn");
-const closeBtn = document.getElementById("closeModal");
-
-openBtn.addEventListener("click", (e) => {
+document.getElementById("moreInfoBtn").addEventListener("click", function (e) {
   e.preventDefault();
-  modal.style.display = "flex";
+  document.getElementById("modal").style.display = "flex";
 });
 
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-window.addEventListener("click", (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none";
-  }
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("modal").style.display = "none";
 });

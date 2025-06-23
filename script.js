ad4abd16c08723ccc61aa44bb8faab567af0daa5
StrataEdition <strata.edition@gmail.com>
2025-06-23 15:44:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var header = document.getElementById('centeredHeader');
     if (content && header) {
       var headerHeight = header.getBoundingClientRect().height;
-      content.style.paddingTop = headerHeight + (headerHeight*0.1);
+      content.style.paddingTop = headerHeight + (headerHeight*0.5);
     }
   }
   setContentPadding();
